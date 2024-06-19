91cdcac21dba18bf072704ac02e17b5eeff14f04
amandasuzzanne <amandasuzanne12@gmail.com>
2024-06-19 13:31:06 +0300

    Complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+("Mr.Chocolate", 2, "M", "Chocolate", "Hairy", 1),
+("Rowdy", 4, "M", "Brown", "Hairy", 1),
+("Tabitha", 6, "F", "Blue", "Hairy", 0),
+("Seargeant Brown", 8, "F", "Chocolate", "Hairy", 1),
+("Melisa", 10, "F", "Brown", "Hairy", 1),
+("Grinch", 3, "M", "Chocolate", "Hairy", 1),
+("Wendy", 1, "F", "Chocolate", "Hairy", 1),
+(NULL, 2, "M", "Chocolate", "Hairy", 1);
